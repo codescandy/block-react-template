10a356a7c74c2be2ba791e35a7d42656d1621ec8
anitaparmar26 <easeanita@gmail.com>
2025-05-08 18:28:55 +0530

    added pages

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppNavbar from './components/common/Navbar';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <AppNavbar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,28 @@
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+function AppNavbar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#">MyReactApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-navbar" />
+        <Navbar.Collapse id="main-navbar">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <NavDropdown title="More" id="more-dropdown">
+              <NavDropdown.Item href="#action/1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/2">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3">Something else</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default AppNavbar;
